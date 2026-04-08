f2b374cb41faafa5455091f2b8d8a8013881ebeb
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-08 16:11:17 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,18 @@
 # utils/logger.py
 
 from datetime import datetime
+import os
+
 
 def log_alert(alert):
-    with open("logs/monitoring.log", "a", encoding="utf-8") as f:
+    log_file = "logs/monitoring.log"
+
+    # Add a blank line ONLY if file already exists and is not empty
+    if os.path.exists(log_file) and os.path.getsize(log_file) > 0:
+        with open(log_file, "a", encoding="utf-8") as f:
+            f.write("\n")
+
+    with open(log_file, "a", encoding="utf-8") as f:
         clean_alert = str(alert).strip()
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         f.write(f"[{timestamp}] {clean_alert}\n")
