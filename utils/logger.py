c9d2694d1d9ee6995c58d85f0a87e1d38ea9866d
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-08 14:22:29 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,5 @@
 # utils/logger.py
 
 def log_alert(alert):
-    with open("logs/monitoring.log", "a") as f:
+    with open("logs/monitoring.log", "a", encoding="utf-8") as f:
         f.write(str(alert) + "\n")
