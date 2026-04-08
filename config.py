1be91c4b884a7948ab6baeb0816496ee2229560e
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-08 13:14:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-# config.py
-
-# Suspicious Parent → Child mappings
 SUSPICIOUS_PARENT_CHILD = {
-    "winword.exe": ["cmd.exe", "powershell.exe", "wscript.exe"],
-    "excel.exe": ["cmd.exe", "powershell.exe"],
-    "outlook.exe": ["cmd.exe", "powershell.exe"],
-    "chrome.exe": ["cmd.exe"],
-    "explorer.exe": ["powershell.exe"]
+    "explorer.exe": ["powershell.exe", "cmd.exe"]
 }
