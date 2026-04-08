08fa51125b42630a529dd54b7fc1fe0f0aa88d07
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-08 15:37:55 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -2,10 +2,8 @@
 
 from datetime import datetime
 
-
 def log_alert(alert):
     with open("logs/monitoring.log", "a", encoding="utf-8") as f:
-        clean_alert = str(alert).strip()  # remove extra spaces/newlines
+        clean_alert = str(alert).strip()
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
         f.write(f"[{timestamp}] {clean_alert}\n")
