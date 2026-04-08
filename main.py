cac2ff5ce0cd273d56eb488154247a1cdf1a4a9b
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-08 13:04:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+# main.py
 
+from core.process_monitor import get_all_processes
+from core.anomaly_detector import detect_parent_child_anomalies
+from utils.logger import log_alert
+
+
+def main():
+    print("🔍 Running Parent-Child Monitoring...\n")
+
+    processes = get_all_processes()
+
+    alerts = detect_parent_child_anomalies(processes)
+
+    if not alerts:
+        print("✅ No suspicious parent-child activity detected.")
+        return
+
+    print("🚨 Suspicious Activities Detected:\n")
+
+    for alert in alerts:
+        print(alert)
+        log_alert(alert)
+
+
+if __name__ == "__main__":
+    main()
