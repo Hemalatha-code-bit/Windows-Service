8cb3f46d74100ae8deb3b3adef232d9c80cab70b
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-08 13:03:07 +0530

    Update process_monitor.py

--- core/process_monitor.py
+++ core/process_monitor.py
@@ -1,1 +1,14 @@
+# core/process_monitor.py
 
+import psutil
+
+def get_all_processes():
+    processes = {}
+
+    for proc in psutil.process_iter(['pid', 'ppid', 'name', 'exe']):
+        try:
+            processes[proc.info['pid']] = proc.info
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+
+    return processes
