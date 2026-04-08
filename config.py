2fe7d7eee0e2c5e85405240565d8638ea44b1b43
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-08 13:17:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,40 @@
+# config.py
+
+# -----------------------------------
+# Suspicious Parent → Child Rules
+# -----------------------------------
 SUSPICIOUS_PARENT_CHILD = {
-    "explorer.exe": ["powershell.exe", "cmd.exe"]
+    "winword.exe": ["cmd.exe", "powershell.exe", "wscript.exe"],
+    "excel.exe": ["cmd.exe", "powershell.exe"],
+    "outlook.exe": ["cmd.exe", "powershell.exe"],
+    "chrome.exe": ["cmd.exe"],  # browser spawning shell = suspicious
 }
+
+# -----------------------------------
+# Suspicious Processes (LOLBins)
+# -----------------------------------
+SUSPICIOUS_CHILD_PROCESSES = [
+    "powershell.exe",
+    "cmd.exe",
+    "wscript.exe",
+    "cscript.exe",
+    "mshta.exe",
+    "rundll32.exe",
+    "regsvr32.exe",
+    "certutil.exe"
+]
+
+# -----------------------------------
+# Safe Processes (to reduce false positives)
+# -----------------------------------
+SAFE_CHILD_PROCESSES = [
+    "chrome.exe",
+    "msedge.exe",
+    "explorer.exe",
+    "onedrive.exe",
+    "ms-teams.exe",
+    "onenotem.exe",
+    "securityhealthsystray.exe",
+    "rtkauduservice64.exe",
+    "git-bash.exe"
+]
